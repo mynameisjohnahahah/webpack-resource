18d8dd0278e58b8384383706690da387a5cb5aff
lxp <=>
2019-08-07 11:15:33 +0800

    'update'

--- customize_prompt.js
+++ customize_prompt.js
@@ -1,41 +1,16 @@
 // 配置选项
 module.exports = [
-    {
-      type: "confirm",
-      name: "router",
-      message: "Install vue-router?",
-      when: function (answers) {
-        return answers.router
-      }
-    },
-    {
-      type: "confirm",
-      name: "vux",
-      message: "是否使用vux？"
-    },{
-      type: "checkbox",
-      name: "env",
-      message: "选择要配置的环境",
-      choices: [
-        {
-          name: 'dev',
-          message: '开发环境',
-          checked: true
-        }, {
-          name: 'pro',
-          message: '生产环境',
-          checked: true
-        }, {
-          name: 'test',
-          message: '测试环境',
-          checked: true
-        }, {
-          name: 'grey',
-          message: '灰度环境',
-        }, {
-          name: 'pre',
-          message: '预发布环境'
-        }
-      ]
+  {
+    type: "confirm",
+    name: "router",
+    message: "是否安装vue-router"
+  },
+  {
+    type: "confirm",
+    name: "flexible",
+    message: "是否使用移动端适配？",
+    when: function (answers) {
+      return answers.mobile
     }
-  ]
+  },
+]
